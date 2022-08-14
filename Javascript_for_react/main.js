1d01728a77173001bbaefc299bce628568325d9f
Mohd-naim <98208008+Mohd-naim@users.noreply.github.com>
2022-08-14 16:06:23 +0530

    some change
    i have added some changes

--- Javascript_for_react/main.js
+++ Javascript_for_react/main.js
@@ -4,6 +4,7 @@ let num = 7;
 const day = 'The code day';
 console.log(num, day);
 
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
 
 // use of function
 function isEven(input) {
@@ -15,12 +16,15 @@ function isEven(input) {
 }
 console.log(isEven(4));
 
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
 
 //use of template litarals
 
 age=24
 const out = 'You are '+age+' Year olds'
 console.log(out);
+
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
 
 //shothand props
 const name = 'Mohd'
@@ -42,11 +46,57 @@ const other = {
 }
 
 let final = Object.assign({},user,other);
+
 console.log({...user,...other});
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
+
 console.log(final);
 
 
 //Arrays 
 const per = ['view user','view report','download report','check report'];
 const other_per = ['init transaction', 'delete user'];
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
+
 console.log(...per,...other_per);
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
+
+
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
+//destructing   -----> Object Destructing 
+
+const person = {
+    favNum:"green",
+    name1:"Naim",
+    cars:['mercedes','toyoto']
+}
+
+// const {favNum,name1,cars}= person
+// console.log(favNum,name1,cars);
+const {cars,favNum,...other1}= person
+console.log(other1);
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
+
+// const {name1:Real, food='chawal' }= person
+// console.log(favNum,name1,cars);
+// console.log(Real,food);
+
+
+console.log("<<<<<<<<<<<<<<--------------------------------------------------------------->>>>>>>>>>>>>>>");
+
+const customer = {
+    cus_nam:'tom',
+    mobile:1234567890,
+    email:'121@gmai.com',
+    addres:{
+        country:'India',
+        city:'Hapur',
+        pincod:'222221',
+        street:'bhan'
+}
+}
+const {...cust} =  customer
+const {addres:{...addres}}= customer
+
+console.log(cust);
+console.log(addres);
